2a8c35065321101604414f3c34479fe219b77832
Elton Rexha <elton.rexha123@outlook.com>
2025-01-30 20:40:11 +0100

    use browserRouter instead

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import router from '@/routes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GettingStartedPage from './pages/GettingStartedPage';
+// import { RouterProvider } from 'react-router-dom';
+// import router from '@/routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GettingStartedPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
