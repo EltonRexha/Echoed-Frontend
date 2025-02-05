879d89b45be5e0670fbd01f3c2fe046f6fadf99c
Elton Rexha <elton.rexha123@outlook.com>
2025-02-05 21:42:02 +0100

    Add a indicator to which description it is

--- src/pages/GettingStartedPage.tsx
+++ src/pages/GettingStartedPage.tsx
@@ -44,6 +44,8 @@ const layoutVariant = {
   },
 };
 
+const LAYOUT_CHANGE_DELAY_MS = 12000;
+
 function GettingStartedPage(): JSX.Element {
   const examplesPart = useRef(null);
   const examplesPartInView = useInView(examplesPart, { once: true });
@@ -120,159 +122,174 @@ function GettingStartedPage(): JSX.Element {
             </div>
           </>
         )}
-        <div className="w-full min-h-[100vh] p-2 sm:p-20 overflow-y-hidden grid gap-2 lg:grid-cols-2 justify-items-center">
-          <div className="self-center">
-            <CycleComponents delay={12000}>
-              <AnimatePresence mode="popLayout">
-                <motion.div
-                  key={1}
-                  variants={layoutVariant}
-                  exit="exit"
-                  animate="animate"
-                  initial="initial"
-                  className="shadow-2xl"
-                >
-                  <img src={postDark} alt="Post" />
-                </motion.div>
-              </AnimatePresence>
-              <AnimatePresence mode="popLayout">
-                <motion.div
-                  key={2}
-                  variants={layoutVariant}
-                  exit="exit"
-                  animate="animate"
-                  initial="initial"
-                  className="shadow-2xl"
-                >
-                  <img src={trendingDark} alt="Trending" />
-                </motion.div>
-              </AnimatePresence>
+        <div className="w-full lg:min-h-[100vh]">
+          <div className="pb-4 pt-20 w-max m-auto flex gap-2 items-center">
+            <CycleComponents delay={LAYOUT_CHANGE_DELAY_MS - 500}>
+              <div className="w-3 h-3 rounded-full bg-gray-200 border-4 border-purple-shade-300"></div>
+              <div className="w-2 h-2 rounded-full bg-gray-200 "></div>
+            </CycleComponents>
+            <CycleComponents delay={LAYOUT_CHANGE_DELAY_MS - 500}>
+              <div className="w-2 h-2 rounded-full bg-gray-200"></div>
+              <div className="w-3 h-3 rounded-full bg-gray-200 border-4 border-purple-shade-300"></div>
             </CycleComponents>
           </div>
-          <div>
-            <CycleComponents delay={12000}>
-              <AnimatePresence mode="popLayout">
-                <motion.div
-                  key={2}
-                  variants={layoutVariant}
-                  exit="exit"
-                  animate="animate"
-                  initial="initial"
-                >
-                  <div className="text-light-secondary dark:text-dark-secondary bg-[color:rgba(255,255,255,0.1)] rounded shadow-2xl p-4  backdrop-blur-2xl">
-                    <h2 className="text-light-primary dark:text-dark-primary text-3xl mb-10 font-semibold">
-                      Create Posts
-                    </h2>
-                    <p className="mb-10">
-                      Echoed allows you to create posts and share your thoughts
-                      with the world. You can write text, share images, videos,
-                      and more to express yourself. Simply click the "Post"
-                      button after composing your message. To make your posts
-                      more engaging, you can easily upload images or videos
-                      alongside your text.
-                    </p>
-                    <ul className="flex flex-col gap-2 mb-10">
-                      <li>
-                        <span className="font-bold">Text</span> Share your
-                        thoughts with just text.
-                      </li>
-                      <li>
-                        <span className="font-bold">Images</span> Add images by
-                        clicking the image icon to select a file from your
-                        device.
-                      </li>
-                      <li>
-                        <span className="font-bold">Videos</span> Upload videos
-                        to make your posts more dynamic.
-                      </li>
-                      <li>
-                        <span className="font-bold">Emojis</span> Use emojis to
-                        express your emotions and make your posts fun!
-                      </li>
-                    </ul>
-                    <p className="mb-5">
-                      Once you're ready, simply hit "Post" and your content will
-                      be shared with your followers.
-                    </p>
-                  </div>
-                </motion.div>
-              </AnimatePresence>
-              <AnimatePresence mode="popLayout">
-                <motion.div
-                  key={3}
-                  variants={layoutVariant}
-                  exit="exit"
-                  animate="animate"
-                  initial="initial"
-                >
-                  <div className="text-light-secondary dark:text-dark-secondary bg-[color:rgba(255,255,255,0.1)] rounded shadow-2xl p-4 backdrop-blur-2xl">
-                    <div className="mb-3">
-                      <h2 className="text-light-primary dark:text-dark-primary text-3xl mb-2 font-semibold">
-                        Trending Topics & Hashtags
+          <div className="overflow-hidden grid gap-2 lg:grid-cols-2 justify-items-center p-2 sm:p-20">
+            <div className="self-center">
+              <CycleComponents delay={LAYOUT_CHANGE_DELAY_MS}>
+                <AnimatePresence mode="popLayout">
+                  <motion.div
+                    key={1}
+                    variants={layoutVariant}
+                    exit="exit"
+                    animate="animate"
+                    initial="initial"
+                    className="shadow-2xl"
+                  >
+                    <img src={postDark} alt="Post" />
+                  </motion.div>
+                </AnimatePresence>
+                <AnimatePresence mode="popLayout">
+                  <motion.div
+                    key={2}
+                    variants={layoutVariant}
+                    exit="exit"
+                    animate="animate"
+                    initial="initial"
+                    className="shadow-2xl"
+                  >
+                    <img src={trendingDark} alt="Trending" />
+                  </motion.div>
+                </AnimatePresence>
+              </CycleComponents>
+            </div>
+            <div>
+              <CycleComponents delay={LAYOUT_CHANGE_DELAY_MS}>
+                <AnimatePresence mode="popLayout">
+                  <motion.div
+                    key={2}
+                    variants={layoutVariant}
+                    exit="exit"
+                    animate="animate"
+                    initial="initial"
+                  >
+                    <div className="text-light-secondary dark:text-dark-secondary bg-[color:rgba(255,255,255,0.1)] rounded shadow-2xl p-4  backdrop-blur-2xl">
+                      <h2 className="text-light-primary dark:text-dark-primary text-3xl mb-10 font-semibold">
+                        Create Posts
                       </h2>
-                      <p>
-                        Stay updated with the latest conversations through
-                        trending topics and hashtags on Echoed. The trending
-                        section highlights popular discussions, events, and
-                        viral moments happening in real-time.
+                      <p className="mb-10">
+                        Echoed allows you to create posts and share your
+                        thoughts with the world. You can write text, share
+                        images, videos, and more to express yourself. Simply
+                        click the "Post" button after composing your message. To
+                        make your posts more engaging, you can easily upload
+                        images or videos alongside your text.
                       </p>
-                    </div>
-
-                    <div className="mb-3">
-                      <h3 className="text-light-primary dark:text-dark-primary text-2xl mb-2">
-                        How Trending Works
-                      </h3>
-                      <p>
-                        Trending topics are determined by a combination of
-                        factors, including the number of posts, engagement
-                        levels, and sudden spikes in activity. The trending list
-                        updates dynamically to reflect what people are talking
-                        about the most.
-                      </p>
-                    </div>
-
-                    <div className="mb-3">
-                      <h3 className="text-light-primary dark:text-dark-primary text-2xl mb-2">
-                        Using Hashtags
-                      </h3>
-                      <ul className="flex flex-col gap-2">
+                      <ul className="flex flex-col gap-2 mb-10">
                         <li>
-                          <span className="font-bold">Create a Hashtag</span>{' '}
-                          Simply type <code>#</code> followed by a word or
-                          phrase (without spaces) to create a hashtag.
+                          <span className="font-bold">Text</span> Share your
+                          thoughts with just text.
                         </li>
                         <li>
-                          <span className="font-bold">Discover Topics:</span>{' '}
-                          Click on a hashtag to explore posts related to that
-                          topic.
+                          <span className="font-bold">Images</span> Add images
+                          by clicking the image icon to select a file from your
+                          device.
                         </li>
                         <li>
-                          <span className="font-bold">Join Conversations:</span>{' '}
-                          Use relevant hashtags in your posts to increase
-                          visibility and engage with a wider audience.
+                          <span className="font-bold">Videos</span> Upload
+                          videos to make your posts more dynamic.
+                        </li>
+                        <li>
+                          <span className="font-bold">Emojis</span> Use emojis
+                          to express your emotions and make your posts fun!
                         </li>
                       </ul>
-                    </div>
-
-                    <div className="mb-3">
-                      <h3 className="text-light-primary dark:text-dark-primary text-2xl mb-2">
-                        Personalized Trends
-                      </h3>
-                      <p>
-                        Echoed offers personalized trending topics based on your
-                        location, interests, and engagement. You can also
-                        explore global trends to see what’s happening worldwide.
-                      </p>
-
-                      <p>
-                        Engage with trending topics to stay in the loop and be
-                        part of the conversation!
+                      <p className="mb-5">
+                        Once you're ready, simply hit "Post" and your content
+                        will be shared with your followers.
                       </p>
                     </div>
-                  </div>
-                </motion.div>
-              </AnimatePresence>
-            </CycleComponents>
+                  </motion.div>
+                </AnimatePresence>
+                <AnimatePresence mode="popLayout">
+                  <motion.div
+                    key={3}
+                    variants={layoutVariant}
+                    exit="exit"
+                    animate="animate"
+                    initial="initial"
+                  >
+                    <div className="text-light-secondary dark:text-dark-secondary bg-[color:rgba(255,255,255,0.1)] rounded shadow-2xl p-4 backdrop-blur-2xl">
+                      <div className="mb-3">
+                        <h2 className="text-light-primary dark:text-dark-primary text-3xl mb-2 font-semibold">
+                          Trending Topics & Hashtags
+                        </h2>
+                        <p>
+                          Stay updated with the latest conversations through
+                          trending topics and hashtags on Echoed. The trending
+                          section highlights popular discussions, events, and
+                          viral moments happening in real-time.
+                        </p>
+                      </div>
+
+                      <div className="mb-3">
+                        <h3 className="text-light-primary dark:text-dark-primary text-2xl mb-2">
+                          How Trending Works
+                        </h3>
+                        <p>
+                          Trending topics are determined by a combination of
+                          factors, including the number of posts, engagement
+                          levels, and sudden spikes in activity. The trending
+                          list updates dynamically to reflect what people are
+                          talking about the most.
+                        </p>
+                      </div>
+
+                      <div className="mb-3">
+                        <h3 className="text-light-primary dark:text-dark-primary text-2xl mb-2">
+                          Using Hashtags
+                        </h3>
+                        <ul className="flex flex-col gap-2">
+                          <li>
+                            <span className="font-bold">Create a Hashtag</span>{' '}
+                            Simply type <code>#</code> followed by a word or
+                            phrase (without spaces) to create a hashtag.
+                          </li>
+                          <li>
+                            <span className="font-bold">Discover Topics:</span>{' '}
+                            Click on a hashtag to explore posts related to that
+                            topic.
+                          </li>
+                          <li>
+                            <span className="font-bold">
+                              Join Conversations:
+                            </span>{' '}
+                            Use relevant hashtags in your posts to increase
+                            visibility and engage with a wider audience.
+                          </li>
+                        </ul>
+                      </div>
+
+                      <div className="mb-3">
+                        <h3 className="text-light-primary dark:text-dark-primary text-2xl mb-2">
+                          Personalized Trends
+                        </h3>
+                        <p>
+                          Echoed offers personalized trending topics based on
+                          your location, interests, and engagement. You can also
+                          explore global trends to see what’s happening
+                          worldwide.
+                        </p>
+
+                        <p>
+                          Engage with trending topics to stay in the loop and be
+                          part of the conversation!
+                        </p>
+                      </div>
+                    </div>
+                  </motion.div>
+                </AnimatePresence>
+              </CycleComponents>
+            </div>
           </div>
         </div>
       </div>
