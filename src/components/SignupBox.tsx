056b83d6cb28378b868f1fc82c71fd3d66c61b1d
Elton Rexha <elton.rexha123@outlook.com>
2025-02-13 21:17:42 +0100

    Add zod, finish personal info form part, and more
    Added and configured ZOD validation with react hook form,
    Wrote the schema of ZOD, finished the personal info
    input group part, made form more responsive, and more

--- src/components/SignupBox.tsx
+++ src/components/SignupBox.tsx
@@ -1,27 +1,74 @@
-import { Gender } from '@/types/gender';
 import { ReactNode, useState } from 'react';
-import { useForm, SubmitHandler, UseFormRegister } from 'react-hook-form';
-import StepDescriptionIndicator from './ui/StepDescriptionIndicator';
 import {
-  Variants,
-  animationControls,
-  motion,
-  useAnimationControls,
-} from 'framer-motion';
+  useForm,
+  SubmitHandler,
+  UseFormRegister,
+  FieldErrors,
+  UseFormWatch,
+} from 'react-hook-form';
+import StepDescriptionIndicator from './ui/StepDescriptionIndicator';
+import { Variants, motion, useAnimationControls } from 'framer-motion';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { countries } from 'countries-list';
+import genders from '@/services/state/genders.json';
 
-interface Inputs {
-  firstName: string;
-  lastName: string;
-  password: string;
-  email: string;
-  confirmPassword: string;
-  username: string;
-  country: string;
-  gender: Gender;
-}
+const COUNTRIES = Object.values(countries)
+  .map((country) => country.name)
+  .sort();
+const MIN_AGE = 13;
+
+const schema = z
+  .object({
+    firstName: z
+      .string()
+      .min(3, 'At least 3 characters')
+      .max(10, 'Reached max of characters (10)')
+      .regex(/^[A-Za-z]+$/, 'Only letters are allowed'),
+    lastName: z
+      .string()
+      .min(3, 'At least 3 characters')
+      .max(10, 'Reached max of characters (10)')
+      .regex(/^[A-Za-z]+$/, 'Only letters are allowed'),
+    username: z
+      .string()
+      .min(3, 'The username must have at least 3 characters')
+      .max(20, 'Reached max of characters'),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(8, 'Password must be at least 8 characters'),
+    confirmPassword: z.string(),
+    country: z.enum(COUNTRIES as [string, ...string[]]),
+    gender: z.enum(genders as [string, ...string[]]),
+    dateOfBirth: z.date().refine((date) => {
+      const today = new Date();
+      const minDate = new Date(
+        today.getFullYear() - MIN_AGE,
+        today.getMonth(),
+        today.getDate()
+      );
+      return date <= minDate;
+    }, `You must be at least ${MIN_AGE} years old`),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  });
+
+type Inputs = z.infer<typeof schema>;
 
 function SignupBox(): JSX.Element {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch
+  } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      dateOfBirth: undefined,
+    },
+  });
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   const [currentInputBox, setCurrentInputBox] = useState(0);
   const incrementCurrentInputBox = (): void => {
@@ -29,18 +76,31 @@ function SignupBox(): JSX.Element {
   };
 
   const inputBoxes: ReactNode[] = [
-    <FirstInputGroup register={register} next={incrementCurrentInputBox} />,
+    <FirstInputGroup
+      register={register}
+      next={incrementCurrentInputBox}
+      errors={errors}
+      watch={watch}
+    />,
     <SecondInputGroup register={register} next={incrementCurrentInputBox} />,
     <ThirdInputGroup register={register} next={incrementCurrentInputBox} />,
   ];
 
   return (
-    <div className="flex flex-col w-[550px] font-raleway p-2 h-[450px] rounded border-dark-secondary dark:border-purple-shade-300 border-2 text-light-primary dark:text-dark-primary">
-      <div className="p-2">
-        <StepDescriptionIndicator
-          currentIndex={currentInputBox}
-          descriptions={['Personal Info', 'Account Info', 'Finish']}
-        />
+    <div className="flex flex-col mt-5 sm:mt-0 w-full sm:w-[650px] font-raleway p-2 sm:h-[650px] rounded border-dark-secondary dark:border-purple-shade-300 border-2 text-light-primary dark:text-dark-primary">
+      <div className="sm:p-2">
+        <div className="hidden sm:block">
+          <StepDescriptionIndicator
+            currentIndex={currentInputBox}
+            descriptions={['Personal Info', 'Account Info', 'Finish']}
+          />
+        </div>
+        <div className="block sm:hidden mb-10 ml-4">
+          <StepDescriptionIndicator
+            currentIndex={currentInputBox}
+            descriptions={['Personal', 'Account', 'Finish']}
+          />
+        </div>
       </div>
       <form onSubmit={handleSubmit(onSubmit)} className="flex-1">
         {inputBoxes[currentInputBox]}
@@ -52,14 +112,28 @@ function SignupBox(): JSX.Element {
 function FirstInputGroup({
   register,
   next,
+  errors,
+  watch,
 }: {
   register: UseFormRegister<Inputs>;
   next: () => void;
+  errors: FieldErrors<Inputs>;
+  watch: UseFormWatch<Inputs>;
 }): JSX.Element {
+  const firstName = watch('firstName');
+  const lastName = watch('lastName');
+  const email = watch('email');
+  const gender = watch('gender');
+  const country = watch('country');
+  const dateOfBirth = watch('dateOfBirth');
+
+  const isFormValid =
+    firstName && lastName && email && gender && country && dateOfBirth;
+
   return (
     <div className="relative h-full flex flex-col">
-      <ul className="flex flex-col gap-3 px-15 justify-center flex-1">
-        <li className="flex gap-5 w-full x">
+      <ul className="flex flex-col gap-3 px-2 sm:px-15 justify-center flex-1">
+        <li className="sm:flex gap-5 w-full x">
           <div className="relative z-0 w-full mb-5 group">
             <input
               id="first_name"
@@ -74,6 +148,11 @@ function FirstInputGroup({
             >
               First Name
             </label>
+            {errors.firstName && (
+              <p className="text-red-600 text-sm font-semibold font-sans">
+                {errors.firstName.message}
+              </p>
+            )}
           </div>
           <div className="relative z-0 w-full mb-5 group">
             <input
@@ -89,6 +168,11 @@ function FirstInputGroup({
             >
               Last Name
             </label>
+            {errors.lastName && (
+              <p className="text-red-600 text-sm font-semibold font-sans">
+                {errors.lastName.message}
+              </p>
+            )}
           </div>
         </li>
 
@@ -107,10 +191,91 @@ function FirstInputGroup({
             >
               Email
             </label>
+            {errors.email && (
+              <p className="text-red-600 text-sm font-semibold font-sans">
+                {errors.email.message}
+              </p>
+            )}
           </div>
         </li>
+        <li className="w-full">
+          <div className="w-full mb-5 flex flex-col gap-2">
+            <label
+              htmlFor="gender"
+              className="text-sm text-gray-500 dark:text-gray-400 "
+            >
+              Gender
+            </label>
+            <select
+              {...register('gender')}
+              id="gender"
+              className="border-b-2 border-gray-300 p-2 dark:bg-purple-shade-400 dark:border-gray-600"
+            >
+              <option value="unkown">Prefer not to say</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+            {errors.gender && (
+              <p className="text-red-600 text-sm font-semibold font-sans">
+                {errors.gender.message}
+              </p>
+            )}
+          </div>
+        </li>
+        <li className="w-full">
+          <div className="w-full mb-5 flex flex-col gap-2">
+            <label
+              htmlFor="country"
+              className="text-sm text-gray-500 dark:text-gray-400 "
+            >
+              Country
+            </label>
+            <select
+              {...register('country')}
+              id="country"
+              defaultValue="United States"
+              className="border-b-2 border-gray-300 p-2 dark:bg-purple-shade-400 dark:border-gray-600"
+            >
+              {COUNTRIES.map((country) => (
+                <option key={country}>{country}</option>
+              ))}
+            </select>
+            {errors.country && (
+              <p className="text-red-600 text-sm font-semibold font-sans">
+                {errors.country.message}
+              </p>
+            )}
+          </div>
+        </li>
+
+        <li className="w-full">
+          <div className="w-full mb-5 flex flex-col gap-2">
+            <label
+              htmlFor="dateOfBirth"
+              className="text-sm text-gray-500 dark:text-gray-400 "
+            >
+              Date of birth
+            </label>
+            <input
+              type="date"
+              {...register('dateOfBirth', {
+                setValueAs: (value) => (value ? new Date(value) : value),
+              })}
+              className="border-b-2 border-gray-300 p-2 dark:bg-purple-shade-400 dark:border-gray-600"
+            />
+          </div>
+          {errors.dateOfBirth && (
+            <p className="text-red-600 text-sm font-semibold font-sans">
+              {errors.dateOfBirth.message === 'Expected date, received string'
+                ? 'Date of birth is required'
+                : errors.dateOfBirth.message}
+            </p>
+          )}
+        </li>
       </ul>
-      <NextButton onClick={next} />
+      {/* Show Next button only if all fields are filled */}
+      {isFormValid && <NextButton onClick={next} />}
     </div>
   );
 }
@@ -183,32 +348,42 @@ function NextButton({ onClick }: { onClick: () => void }): JSX.Element {
   const arrowAnimateControls = useAnimationControls();
 
   return (
-    <div className="flex gap-1 pr-15 mb-10 ml-auto ">
-      <motion.button
-        variants={buttonVariants}
-        initial="initial"
-        whileHover='whileHover'
-        className="font-bold font-sans cursor-pointer z-10"
-        onClick={onClick}
-        onMouseEnter={() => {
-          arrowAnimateControls.start('animate');
-        }}
-        onMouseLeave={() => {
-          arrowAnimateControls.start('initial');
-        }}
-        type="button"
-      >
-        Next
-      </motion.button>
-      <motion.p
-        className="text-purple-shade-100"
-        variants={arrowVariants}
-        animate={arrowAnimateControls}
-        initial="initial"
-      >
-        &gt;
-      </motion.p>
-    </div>
+    <>
+      <div className="block sm:hidden">
+        <button
+          className="font-bold font-sans border-2 p-2 border-purple-shade-200 w-full rounded-1xl"
+          type="button"
+        >
+          Next
+        </button>
+      </div>
+      <div className="hidden sm:flex gap-1 pr-15 mb-10 ml-auto ">
+        <motion.button
+          variants={buttonVariants}
+          initial="initial"
+          whileHover="whileHover"
+          className="font-bold font-sans cursor-pointer z-10"
+          onClick={onClick}
+          onMouseEnter={() => {
+            arrowAnimateControls.start('animate');
+          }}
+          onMouseLeave={() => {
+            arrowAnimateControls.start('initial');
+          }}
+          type="button"
+        >
+          Next
+        </motion.button>
+        <motion.p
+          className="text-purple-shade-100"
+          variants={arrowVariants}
+          animate={arrowAnimateControls}
+          initial="initial"
+        >
+          &gt;
+        </motion.p>
+      </div>
+    </>
   );
 }
 
