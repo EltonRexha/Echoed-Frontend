53bcce37f627a4111897c2085e82d75f4c392b3f
Elton Rexha <elton.rexha123@outlook.com>
2025-03-03 21:33:18 +0100

    Add axios interceptor to handle unathenticated error responses

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -6,7 +6,7 @@ function SignupPage(): JSX.Element {
   return (
     <div className="overflow-x-hidden font-raleway bg-lighter dark:bg-purple-shade-400">
       <FadeIn>
-        <div className="min-h-[101vh] bg-lighter dark:bg-purple-shade-400 pl-2 pr-2 sm:pl-10 sm:pr-10 relative flex flex-col">
+        <div className="min-h-[100vh] bg-lighter dark:bg-purple-shade-400 pl-2 pr-2 sm:pl-10 sm:pr-10 relative flex flex-col">
           <GettingStartedNav />
           <div className="w-full flex flex-col sm:items-center sm:justify-center flex-1">
             <Outlet />
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,4 +4,40 @@ const axios = Axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
+const MAX_RETRIES = 1;
+let retryCount = 0;
+
+axios.interceptors.response.use(
+  (success) => {
+    return success;
+  },
+  async (error) => {
+    const response = error.response;
+    const config = error.config;
+    if (response.status === 401) {
+      if (retryCount >= MAX_RETRIES) {
+        //reset retryCount for the next time
+        retryCount = 0;
+
+        //In case refresh didn't work we might as well logout user
+        await axios.post('/auth/logout', null, {
+          withCredentials: true,
+        });
+
+        return Promise.reject(error);
+      }
+
+      retryCount += 1;
+
+      //try to refresh token
+      await axios.post('/auth/refresh', null, {
+        withCredentials: true,
+      });
+
+      return axios(config);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axios;
