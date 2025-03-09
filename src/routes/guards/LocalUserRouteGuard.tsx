e20615149a96ff2504a6279e37980cbd086a6af9
Elton Rexha <elton.rexha123@outlook.com>
2025-03-09 01:11:39 +0100

    fix order of guards being wrong

--- src/routes/guards/LocalUserRouteGuard.tsx
+++ src/routes/guards/LocalUserRouteGuard.tsx
@@ -1,13 +1,10 @@
-
 import { RootState } from '@/services/state/redux/store';
 import { ReactNode } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-//Routes which will only be available to local users and not OAuth users
-//As OAuth users will be prompted to a seperate page to finsih their account
-//Which will create for them a local user
-export default function LocalUserRoute({ children }: { children: ReactNode }) {
+//Routes which will only be available to OAuth users
+export default function OAuthUserRoute({ children }: { children: ReactNode }) {
   const { isAuthenticated: isAuth, user } = useSelector(
     (state: RootState) => state.Authentication
   );
@@ -16,8 +13,8 @@ export default function LocalUserRoute({ children }: { children: ReactNode }) {
     return <Navigate to={'/log-in'} />;
   }
 
-  if (user && (user.UserType === 'github' || user.UserType === 'google')) {
-    return <Navigate to={'/account/complete-profile'} />;
+  if (user && user.UserType === 'local') {
+    return <Navigate to={'/home'} />;
   }
 
   return children;
